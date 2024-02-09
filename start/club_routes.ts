8a9afcf05061b468abf7bcc1fcf9e943690ab551
bertyn99 <bboulikou@gmail.com>
2024-02-09 14:41:44 +0100

    Refactor club routes and add authentication middleware

--- start/club_routes.ts
+++ start/club_routes.ts
@@ -1,4 +1,6 @@
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+
 const ClubsController = () => import('#controllers/clubs_controller')
 
 export default function clubRoutes() {
@@ -6,9 +8,18 @@ export default function clubRoutes() {
     .group(() => {
       router.get('/', [ClubsController, 'index']) // Afficher tous les clubs
       router.get('/:id', [ClubsController, 'show']) // Afficher un club individuel
+    }).prefix('/clubs')
+
+  router
+    .group(() => {
       router.post('/', [ClubsController, 'create']) // Créer un nouveau club
       router.put('/:id', [ClubsController, 'update']) // Mettre à jour un club individuel
       router.delete('/:id', [ClubsController, 'destroy']) // Supprimer un club
     })
-    .prefix('/club')
+    .use(middleware.auth({
+      guards: ['api']
+    }))
+    .prefix('/clubs')
+
+
 }
