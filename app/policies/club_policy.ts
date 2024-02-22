b78ad75dcc40dae2b72ef591a19dfb7f03189c18
bertyn99 <bboulikou@gmail.com>
2024-02-22 15:05:11 +0100

    Add ClubPolicy and update main.ts

---
+++ app/policies/club_policy.ts
@@ -0,0 +1,29 @@
+import User from '#models/user'
+import Club from '#models/club'
+import { BasePolicy } from '@adonisjs/bouncer'
+import { AuthorizerResponse } from '@adonisjs/bouncer/types'
+
+export default class ClubPolicy extends BasePolicy {
+
+    public async viewList(user: User): Promise<AuthorizerResponse> {
+
+        return true
+    }
+    public async view(user: User, club: Club): Promise<AuthorizerResponse> {
+
+        return true
+    }
+
+    public async create(user: User): Promise<AuthorizerResponse> {
+
+        return true
+    }
+
+    public async update(user: User, club: Club): Promise<AuthorizerResponse> {
+        // check if the user is a menber and if  admin of the club
+        if (user.memberships && user.memberships.find(membership => membership.id === club.id && membership.role === 'admin')) {
+            return true
+        }
+        return false
+    }
+}
