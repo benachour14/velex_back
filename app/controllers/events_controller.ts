c4050845a72bd3b3bb31813b996d46245a09dee3
guillaume <legoff.guill@gmail.com>
2024-02-22 14:37:04 +0100

    Ajouter les méthodes CRUD pour les événements dans EventsController

--- app/controllers/events_controller.ts
+++ app/controllers/events_controller.ts
@@ -1,4 +1,43 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import { HttpContext } from '@adonisjs/core/http'
 
 export default class EventsController {
+  async index({ response }: HttpContext) {
+    return response.json('events/index')
+  }
+
+  async create({ response }: HttpContext) {
+    return response.json('events/create')
+  }
+
+  async store({ request, response }: HttpContext) {
+    const data = request.only([
+      'title',
+      'description',
+      'location',
+      'start_date',
+      'end_date',
+      'club_id',
+    ])
+  }
+
+  async show({ response }: HttpContext) {
+    return response.json('events/show')
+  }
+
+  async edit({ response }: HttpContext) {
+    return response.json('events/edit')
+  }
+
+  async update({ request, response }: HttpContext) {
+    const data = request.only([
+      'title',
+      'description',
+      'location',
+      'start_date',
+      'end_date',
+      'club_id',
+    ])
+  }
+
+  async destroy({ response }: HttpContext) {}
 }
