5b3bd653570f15fe285a6a884d4915162f649900
bertyn99 <bboulikou@gmail.com>
2024-02-22 14:10:11 +0100

    Add club_memberships table migration

--- database/migrations/1708607223490_create_club_memberships_table.ts
+++ database/migrations/1708607223490_create_club_memberships_table.ts
@@ -1,12 +1,13 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'events'
+  protected tableName = 'club_memberships'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('club_id').unsigned().references('id').inTable('clubs').onDelete('CASCADE')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
