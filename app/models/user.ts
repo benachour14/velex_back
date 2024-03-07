be4a17587a56b39070dfbdbe9de49e9f6415b59e
bertyn99 <bboulikou@gmail.com>
2024-03-07 16:59:49 +0100

    Add role property to User model

--- app/models/user.ts
+++ app/models/user.ts
@@ -26,6 +26,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column()
   declare password: string
 
+  @column()
+  declare role: 'USER'|'ADMIN'
+
   @manyToMany(() => Club, {
     pivotTable: 'club_memberships',
   })
