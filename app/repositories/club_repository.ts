d5cc05f50ad7c549202f0bebcf6b8525917f34f7
guillaume <legoff.guill@gmail.com>
2024-04-25 13:50:50 +0200

    Ajouter le fichier club_repository.ts pour gérer les opérations sur les clubs

---
+++ app/repositories/club_repository.ts
@@ -0,0 +1,40 @@
+import Club from '#models/club'
+import { PortClubRepository } from './base_repository'
+
+export class ClubRepository implements PortClubRepository {
+  create(data: any): Promise<Club> {
+    return Club.create(data)
+  }
+
+  update(id: number, data: any): Promise<Club | null> {
+    const club = Club.find(id)
+    if (club) {
+      club.merge(data)
+      return club.save()
+    }
+    return null
+  }
+
+  delete(id: number): Promise<void> {
+    const club = Club.find(id)
+    if (club) {
+      return club.delete()
+    }
+  }
+
+  async findByName(name: string): Promise<Club | null> {
+    return Club.findOneBy('name', name)
+  }
+
+  async findClubsWithMembers(): Promise<Club[]> {
+    return Club.query().with('members').fetch()
+  }
+
+  async findById(id: number): Promise<Club | null> {
+    return Club.find(id)
+  }
+
+  async find(): Promise<Club[]> {
+    return await Club.all()
+  }
+}
