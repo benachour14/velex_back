6e7d8218bee8c256168c8780a8d8647af85aa61a
bertyn99 <bboulikou@gmail.com>
2024-04-25 14:52:46 +0200

    Refactor user.spec.ts to use FakeUserRepository in tests

---
+++ tests/functional/service/user.spec.ts
@@ -0,0 +1,65 @@
+import User from '#models/user'
+import PortUserRepository from '#repositories/interfaces/user_interface'
+import UserService from '#services/user_service'
+import app from '@adonisjs/core/services/app'
+import { test } from '@japa/runner'
+
+test.group('user', () => {
+  const fakeUser = [
+    {
+      fullName: 'aa',
+      email: 'fake@gmail.com',
+      password: '123456',
+      password_confirmation: '123456',
+    },
+    {
+      fullName: 'bb',
+      email: 'fake@gmail.com',
+      password: '123456',
+      password_confirmation: '123456',
+    },
+  ]
+
+  class FakeUserRepository implements PortUserRepository {
+    find(): Promise<User[]> {}
+    async create(item: Partial<User>): Promise<User> {
+      return (await item) as User
+    }
+    update(id: any, item: Partial<User>): Promise<User> {
+      throw new Error('Method not implemented.')
+    }
+    delete(id: any): Promise<User> {
+      throw new Error('Method not implemented.')
+    }
+    findById(id: any): Promise<User | null> {
+      throw new Error('Method not implemented.')
+    }
+    findByName(name: string): Promise<User | null> {
+      throw new Error('Method not implemented.')
+    }
+    findUsersWithMembers(): Promise<User[]> {
+      throw new Error('Method not implemented.')
+    }
+    verifyCredentials(email: string, password: string): Promise<User | null> {
+      throw new Error('Method not implemented.')
+    }
+    createToken(user: User): Promise<any> {
+      throw new Error('Method not implemented.')
+    }
+  }
+  app.container.swap(PortUserRepository, () => {
+    return new FakeUserRepository()
+  })
+
+  test('create user', async ({ assert }) => {
+    const fakeUser = {
+      fullName: 'aa',
+      email: 'fake@gmail.com',
+      password: '123456',
+      password_confirmation: '123456',
+    }
+    const userService = new UserService(new FakeUserRepository())
+    const user = await userService.createUser(fakeUser)
+    assert.deepEqual(user, fakeUser)
+  })
+})
