7fb6a3f4c52f886ba09e5f3c6d8b9c33203c74e8
bertyn99 <bboulikou@gmail.com>
2024-09-22 14:48:56 +0200

    fix validator

--- app/validators/event_validator.ts
+++ app/validators/event_validator.ts
@@ -4,7 +4,14 @@ export const eventValidator = vine.compile(
   vine.object({
     title: vine.string().trim().minLength(3).maxLength(255).nullable(),
     description: vine.string().trim().optional().nullable(),
-    location: vine.string().trim().maxLength(255).nullable(),
+    location: vine.object({
+      city: vine.string().nullable(),
+      country: vine.string().nullable(),
+      state: vine.string().nullable(),
+      postalCode: vine.string().nullable(),
+      latitude: vine.number().nullable(),
+      longitude: vine.number().nullable(),
+    }),
     start_date: vine.date(),
     end_date: vine.date().optional(),
     club_id: vine.number(),
